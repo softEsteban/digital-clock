aace88e1b7865cf3a699f747372f175656d488f5
Esteban Toro <estebantoro.greenman@gmail.com>
2022-03-21 15:03:17 +0100

    consuming worldtimeapi and print to dom

--- scripts.js
+++ scripts.js
@@ -1,53 +1,72 @@
-// let date = new Date().toLocaleDateString();
-// console.log(date);
-// let date1 = new Date().toDateString();
-// console.log(date1);
-// let time = new Date().toTimeString();
-// console.log(time);
-// let time2 = new Date().toLocaleTimeString();
-// console.log(time2);
-
 function getCityTime(continent, city) {
-  axios
-    .get(`http://worldtimeapi.org/api/timezone/${continent}/${city}`)
-    .then((res) => {
-      let dateObj = res.data;
-      let dateTime = dateObj.datetime;
-      //2022-03-21T13:42:13.826454+09:00 format
-      // console.log(dateObj);
-      // console.log(dateTime);
-      let slice = dateTime.slice(11, 19)
-      // console.log(slice)
-      return slice;
+  setInterval(() => {
+    axios
+      .get(`http://worldtimeapi.org/api/timezone/${continent}/${city}`)
+      .then((res) => {
+        let dateObj = res.data;
+        let dateTime = dateObj.datetime;
+        let slice = dateTime.slice(11, 19);
 
-    })
-    .catch((err) => {
-      return console.log(err);
-    });
-    
+        switch (city) {
+          case "Tokyo":
+            document.getElementById("japan-time-span").innerHTML =
+              " " + slice + " ";
+            console.log(slice);
+            break;
+
+          case "Buenos_Aires":
+            document.getElementById("arg-time-span").innerHTML =
+              " " + slice + " ";
+            console.log(slice);
+            break;
+
+          case "Berlin":
+            document.getElementById("ger-time-span").innerHTML =
+              " " + slice + " ";
+            console.log(slice);
+            break;
+
+          case "Bogota":
+            document.getElementById("col-time-span").innerHTML =
+              " " + slice + " ";
+            console.log(slice);
+            break;
+
+          default:
+            break;
+        }
+      })
+      .catch((err) => {
+        return console.log(err);
+      });
+  }, 1000);
 }
 
-function initClocks()
-{
+function initClocks() {
   //Local clock
   setInterval(() => {
     document.getElementById("my-time-span").innerHTML =
       " " + new Date().toLocaleTimeString() + " ";
   }, 1000);
 
-  //Tokyo Clock
-  // let timeT = getCityTime("Asia", "Tokyo")
-  // setTimeout(() => {
-  //   console.log(timeT)
-  //   document.getElementById("japan-time-span").innerHTML =
-  //      " " + timeT  + " ";
-  // }, 5000);
+  //Local date
+  getDateInDom = () => {
+    document.getElementById("my-date-span").innerHTML =
+      new Date().toDateString();
+  };
 
+  //Tokyo Clock
+  getCityTime("Asia", "Tokyo");
+
+  //Buenos Aires Clock
+  getCityTime("America/Argentina", "Buenos_Aires");
+
+  //Buenos Aires Clock
+  getCityTime("Europe", "Berlin");
+
+  //Bogota Clock
+  getCityTime("America", "Bogota");
 }
 
-//Start
-getDateInDom = () => {
-  document.getElementById("my-date-span").innerHTML = new Date().toDateString();
-};
-getDateInDom();
 initClocks();
+
